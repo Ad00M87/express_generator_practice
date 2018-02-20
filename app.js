ae5f8f5a77ac6062632b69c8b5f92e435fdd4207
Adam Kimball <adamchasekimball@gmail.com>
2018-02-20 11:50:07 -0700

    routing edit and delete

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var movies = require('./routes/movies');
 
 var app = express();
@@ -19,12 +19,14 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/movies', movies);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var router = express.Router();
-
-var movies = [
-  { id: 1, title: 'Oceans 11' },
-  { id: 2, title: 'The Hobbit' },
-  { id: 3, title: 'Who framed Roger Rabbit' }
-]
+var Movie = require('../models').Movie;
 
 /* GET movie listings. */
 router.get('/', function(req, res) {
-  res.render('movies', { movies: movies });
+  Movie.all()
+    .then( function(movies) {
+      return res.render('movies', { movies: movies });
+  })
+});
+
+/* POST add movie listing */
+router.post('/', function(req, res) {
+  var title = req.body.title;
+  Movie.create({ title: title })
+    .then( function() {
+      res.redirect('/movies');
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  Movie.findById(req.params.id)
+    .then( function(movie) {
+      movie.destroy()
+    })
+    .then( function() {
+      return res.redirect('/movies');
+  });
+});
+
+router.get('/:id/edit', function(req, res) {
+  Movie.findById(req.params.id)
+    .then( function(movie) {
+      return res.render('edit', { movie: movie });
+  });
 });
 
 module.exports = router;
